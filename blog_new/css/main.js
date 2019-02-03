8fccd615e661d4d73e2b7052151cf8b2d140433c
jakexxwilers <46679386+jakexxwilers@users.noreply.github.com>
2019-02-03 05:07:26 +0100

    commit_of_blog

---
+++ blog_new/css/main.js
@@ -0,0 +1,14 @@
+$('.suzette_js').click(function(){
+    $('.imgone').toggle();
+
+});
+
+$('.metro_js').click(function(){
+    $('.imgtwo').toggle();
+
+});
+
+$('.vitaly_js').click(function(){
+    $('.imgthree').toggle();
+
+});
